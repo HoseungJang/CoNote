50f52cb758d2707b967fb76e74827a7775874f74
HoseungJang <hsjang.dev@gmail.com>
2020-06-08 17:19:04 +0900

    UPDATE: language

--- src/public/js/editor.js
+++ src/public/js/editor.js
@@ -2,6 +2,6 @@ require.config({ paths: { 'vs': '/monaco-editor/min/vs' } });
 
 require(['vs/editor/editor.main'], function () {
     const editor = monaco.editor.create(document.getElementById('container'), {
-        language: 'typescript'
+        language: 'javascript'
     });
 });
