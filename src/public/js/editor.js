0fb8827cf30d582d34ae5d311385acdbaa3b954c
HoseungJang <hsjang.dev@gmail.com>
2020-06-09 11:36:24 +0900

    ADD: socket event

--- src/public/js/editor.js
+++ src/public/js/editor.js
@@ -1,19 +1,28 @@
 let editor;
+let isChangedBySocket = false;
 
 require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.20.0/min/vs' } });
 
 require(['vs/editor/editor.main'], function () {
-    //const socket = io.on("/");
+    const socket = io("/");
 
     editor = monaco.editor.create(document.getElementById('container'), {
         language: "javascript",
     });
 
-    /*editor.onDidChangeModelContent(e => {
-        socket.emit("changeContent", e.change);
+    editor.onDidChangeModelContent(e => {
+        if (isChangedBySocket) {
+            isChangedBySocket = false;
+        } else {
+            socket.emit("changeContent", e);
+        }
     });
 
-    socket.on("changeContent", (change) => {
-        editor.getModel().applyEdits(change);
-    });*/
+    socket.on("changeContent", (e) => {
+        const selection = editor.getSelection();
+
+        isChangedBySocket = true;
+        editor.getModel().applyEdits(e.changes);
+        editor.setSelection(selection);
+    });
 });
