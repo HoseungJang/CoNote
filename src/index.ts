5e4d37f1166bc8cb1cd844f44cb43590afa40ae6
HoseungJang <hsjang.dev@gmail.com>
2020-06-09 11:38:28 +0900

    ADD: socket server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { App } from "./app";
+import { Socket } from "./socket";
 
 const app: App = new App();
+const socket: Socket = new Socket(app.getServer());
 
 ((app: App) => {
     app.listen(80);
